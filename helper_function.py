179d3f5766c04711678d4de67385c6c67ffc1998
Mridul <mridulrao674385@gmail.com>
2024-11-05 13:55:24 -0800

    main helper function

---
+++ helper_function.py
@@ -0,0 +1,11 @@
+# this is a helper function file
+# it contains the helper functions for the project 
+
+def load_data():
+    pass
+
+def preprocess_data():
+    pass
+
+def train_model():
+    pass
